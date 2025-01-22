d989b5eee4f9376686334a64992f69af84bfca77
Denis Vasilenko <ghostinchip@gmail.com>
2025-01-22 22:40:05 +0300

    Tasks 1, 2 are done

--- Exercises/2-key.js
+++ Exercises/2-key.js
@@ -1,9 +1,27 @@
 'use strict';
 
-const generateKey = (length, possible) => {
-  // Generate string of random characters
-  // Use Math.random() and Math.floor()
-  // See documentation at MDN
+const characters = () => {
+  // some text about function
+  const temp = [];
+  for (let i = 48; i < 91; i++) {
+    if (i > 57 && i < 65) continue;
+    temp.push(String.fromCharCode(i).toLowerCase());
+  }
+  return temp;
 };
+
+const generateKey = (length, possible) => {
+  // some text about function
+  let key = '';
+  for (let i = 0; i < length; i++) {
+    const random = Math.floor(Math.random() * 25) + 1;
+    key += possible[random];
+  }
+  return key;
+};
+
+const chars = characters();
+const key = generateKey(16, chars);
+console.log(key);
 
 module.exports = { generateKey };
